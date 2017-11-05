d4f8be3c28eed8aa7f41240639deb027735add34
Ripal Shah <ripal.1021@gmail.com>
2017-11-05 12:07:51 +0530

    Initial commit

---
+++ app/src/test/java/com/ripal/whatstheweather/DownloadTaskTest.java
@@ -0,0 +1,21 @@
+package com.ripal.whatstheweather;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ripal on 15/Oct/17.
+ */
+public class DownloadTaskTest {
+    @Test
+    public void doInBackground() throws Exception {
+
+    }
+
+    @Test
+    public void onPostExecute() throws Exception {
+
+    }
+
+}
